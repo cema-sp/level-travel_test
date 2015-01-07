334ab985378389953ef841193c7055dd1f77de47
cema-sp <cema.rus@gmail.com>
2015-01-07 19:48:36 +0300

    Removing pry for Travic-CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ require 'rspec/rails'
 require 'capybara/rails'
 require 'webmock/rspec'
 require 'sidekiq/testing'
-require 'pry'
+require 'pry' unless ENV['ON_TRAVIS']
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
