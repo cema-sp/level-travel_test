97a5c8b55025ebd146a8873f87cde362176be8c3
cema-sp <cema.rus@gmail.com>
2015-01-11 23:09:48 +0300

    Fixing images path

--- lib/kramdown_converter_html.rb
+++ lib/kramdown_converter_html.rb
@@ -1,9 +1,16 @@
+
 module Kramdown
   module Converter
     class Html
       alias_method :convert_img_original, :convert_img
       def convert_img(el, indent)
-        el.attr["src"].sub!("app/assets/images/", "assets/")
+        extend ActionView::Helpers::AssetUrlHelper
+        # binding.pry
+        if el.attr["src"].include?('app/assets/images/') &&
+          (matching = el.attr["src"].match(/\w+\.\w+$/))
+
+          el.attr["src"] = image_path(matching[0])
+        end
         convert_img_original(el, indent)
       end
     end
--- spec/lib/kramdown_converter_html_spec.rb
+++ spec/lib/kramdown_converter_html_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 require Rails.root.join('lib/kramdown_converter_html')
 
 RSpec.describe Kramdown::Converter::Html do
-  let(:img_md) { '![Image](app/assets/images/img.png)' }
-  let(:img_html) { '<img src="assets/img.png" alt="Image" />' }
+  before { extend ActionView::Helpers::AssetUrlHelper }
+
+  let(:img_name) { 'img.png' }
+  let(:img_asset_path) { image_path(img_name) }
+  let(:img_md) { "![Image](app/assets/images/#{img_name})" }
+  let(:img_html) { "<img src=\"#{img_asset_path}\" alt=\"Image\" />" }
 
   describe '#convert_img(el, indent)' do
     it 'uses custom src processing' do
+      # binding.pry
       expect(Kramdown::Document.new(img_md).to_html).to match(img_html)
     end
   end
